3e00a1a418d73a1ed36d04155d5405aa24481293
Srijan Dahal <41977413+Srijan1231@users.noreply.github.com>
2024-03-10 00:44:16 +1100

    Time: 77 ms (22.62%), Space: 50.7 MB (84.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -5,7 +5,7 @@
 var missingNumber = function(nums) {
     let sNums = nums.sort(function (a, b) {  return a - b;  })
    let res = 0
-   console.log(sNums)
+   
    for(let i=0; i<sNums.length; i++){
        if(sNums[i] != i + 1 ){
            res = i + 1
